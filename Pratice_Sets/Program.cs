2a3030507308d4a8f121e7ea37f2fcfb34e84df4
pedro-mileipp <poncianopedro70@gmail.com>
2022-11-23 17:25:29 -0300

    finaliza HashSet e SortedSet, conjuntos em C#

---
+++ Pratice_Sets/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Pratice_Sets;
+
+/*
+
+==> Problema
+
+Um site de internet registra um log de acessos dos usuários. Umregistro de log consiste no nome de usuário e o instante em que o usuário acessou o site no padrão ISO 8601, separados por espaço, conforme exemplo. Fazer um programa que leia o log de acessos apartir de um arquivo, e daí informe quantos usuários distintosacessaram o site.
+
+input file:
+amanda 2020-08-26T20:45:08
+alex86 2020-08-26T21:49:37
+bobbrown 2020-08-27T03:19:13
+amanda 2020-08-27T08:11:00
+jeniffer3 2020-08-27T09:19:24
+alex86 2020-08-27T22:39:52
+amanda 2020-08-28T07:42:19
+
+Execution:
+Enter file full path: c:\temp\in.txt
+Total users: 4
+
+*/
+
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { Username = name, Instant = instant });
+                    }
+                    Console.WriteLine("Total users: " + set.Count);
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
